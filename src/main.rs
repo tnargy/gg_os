e336c3c93ab206fdd6deef55dfb3d54038cddbd4
Grant Gandy <clsscmstng@gmail.com>
2018-08-15 16:07:12 -0500

    uploading sourcecode

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+#![feature(panic_implementation)] // required for defining the panic handler
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+extern crate bootloader_precompiled;
+extern crate volatile;
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+//static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    /*
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;            
+        }
+    }
+     */
+
+    vga_buffer::print_something();
+    loop {}
+}
