39663a956749a0e1201d6838258b948973c5bd43
Grant Gandy <clsscmstng@gmail.com>
2018-08-16 12:36:49 -0500

    Unit Tests for VGA Buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 #![feature(panic_implementation)] // required for defining the panic handler
 #![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
+
+#[cfg(test)]
+extern crate std;
+
+#[cfg(test)]
+extern crate array_init;
 
 #[macro_use]
 extern crate lazy_static;
@@ -12,20 +19,18 @@ use core::panic::PanicInfo;
 #[macro_use]
 mod vga_buffer;
 
+#[cfg(not(test))]  // only compile when test flag is not set
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    //use core::fmt::Write;
-    //vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    //write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
     println!("Hello World{}", "!");
-    panic!("Some panic message");
-    
+        
     loop {}
 }
 
-/// This function is called on panic.
+#[cfg(not(test))]  // only compile when test flag is not set
 #[panic_implementation]
 #[no_mangle]
+/// This function is called on panic.
 pub fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
