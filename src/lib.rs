ccd3babe09c59e75b8513c82bdc6a3b905b13a37
Grant Gandy <clsscmstng@gmail.com>
2018-08-17 12:17:31 -0500

    Added Double Fault Exception Handling

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ extern crate std;
 
 pub mod vga_buffer;
 pub mod serial;
+pub mod gdt;
 
 pub unsafe fn exit_qemu() {
     use x86_64::instructions::port::Port;
--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,8 @@ use x86_64::structures::idt::{InterruptDescriptorTable, ExceptionStackFrame};
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    gg_os::gdt::init();
     init_idt();
-
-    // invode a breakpoint exception
-    x86_64::instructions::int3();
 
     println!("It did not crash!");
     loop {}
@@ -41,6 +39,11 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable  = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gg_os::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        
         idt
     };
 }
@@ -55,4 +58,12 @@ extern "x86-interrupt" fn breakpoint_handler(
     stack_frame: &mut ExceptionStackFrame)
 {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+/// Create Double Fault handler
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut ExceptionStackFrame, _error_code: u64)
+{
+    println!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    loop {}
 }
