603f8a13eebb027cbf82859c35af669d3d2efbe6
Grant Gandy <clsscmstng@gmail.com>
2018-08-21 14:05:29 -0500

    Working ahead: added HW Timer

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate volatile;
 extern crate lazy_static;
 extern crate uart_16550;
 extern crate x86_64;
+extern crate pic8259_simple;
 
 #[cfg(test)]
 extern crate array_init;
@@ -17,6 +18,7 @@ extern crate std;
 pub mod gdt;
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 pub unsafe fn exit_qemu() {
     use x86_64::instructions::port::Port;
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,11 @@ pub extern "C" fn _start() -> ! {
 
     gg_os::gdt::init();
     init_idt();
+    unsafe { gg_os::interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 
+    x86_64::instructions::int3();
+    
     println!("It did not crash!");
     loop {}
 }
@@ -44,7 +48,9 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gg_os::gdt::DOUBLE_FAULT_IST_INDEX);
         }
-
+        let timer_interrupt_id = usize::from(gg_os::interrupts::TIMER_INTERRUPT_ID);
+        idt[timer_interrupt_id].set_handler_fn(timer_interrupt_handler);
+            
         idt
     };
 }
@@ -57,6 +63,15 @@ pub fn init_idt() {
 /// Create Exception Breakpoint handler
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+/// Create Timer Interrupt handler
+extern "x86-interrupt" fn timer_interrupt_handler(
+    _stack_frame: &mut ExceptionStackFrame)
+{
+    // PIC expects an explicit "end of interrupt" (EOI) signal
+    unsafe { gg_os::interrupts::PICS.lock().notify_end_of_interrupt(
+        gg_os::interrupts::TIMER_INTERRUPT_ID) }
 }
 
 /// Create Double Fault handler
